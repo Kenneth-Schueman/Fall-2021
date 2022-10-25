dab7a846cf15378779bc3b41f59600fa99ef9726
Kenneth Schueman <108905733+Kenneth-Schueman@users.noreply.github.com>
2022-10-24 22:22:27 -0500

    Lab 01

---
+++ Lab01/Soruce Code/lab01-1.c
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------
+-		    SE 185: Lab 01 - Getting to Know the 185 Environment		 	 -
+-	Name: Kenneth Schueman																	 -
+- 	Section: 6																 -
+-	NetID: kenschue																     -
+-	Date: 9/2/2021																	 -
+-----------------------------------------------------------------------------*/
+
+/*
+	Here is a block comment.
+	These lines don't run when you compile or run the code.
+	I.e., they're the machine doesn't see them, only you.
+*/
+
+/*----------------------------------------------------------------------------
+-								Includes									 -
+-----------------------------------------------------------------------------*/
+#include <stdio.h>
+
+/*----------------------------------------------------------------------------
+-								Implementation								 -
+-----------------------------------------------------------------------------*/
+int main(int argc, char *argv[])
+{
+    // This is a C comment, this line doesn't run in the program
+
+    printf("Hello World!"); // Modify this line
+    printf("\n"); // This prints a newline character
+
+    return 0; // This is a return statement
+}
